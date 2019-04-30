d96b2d34bd6772ee6609db581ec70bef8ab9274b
DiZhao <zd277607529@gmail.com>
2019-05-01 09:35:26 +1200

    update

--- Chapter1/Housing.py
+++ Chapter1/Housing.py
@@ -5,6 +5,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import hashlib
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer
+from pandas.plotting import scatter_matrix
+
 
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
@@ -39,34 +47,97 @@ housing = load_housing_data()
 # plt.show()
 
 
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
+# def split_train_test(data, test_ratio):
+#     shuffled_indices = np.random.permutation(len(data))
+#     test_set_size = int(len(data) * test_ratio)
+#     test_indices = shuffled_indices[:test_set_size]
+#     train_indices = shuffled_indices[test_set_size:]
+#     return data.iloc[train_indices], data.iloc[test_indices]
 
 
 # train_set, test_set = split_train_test(housing, 0.2)
 # print(len(train_set),"train +", len(test_set), "test")
 
 
-def test_set_check(identifier, test_ratio, hash):
-    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+# def test_set_check(identifier, test_ratio, hash):
+#     return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+#
+#
+# def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+#     ids = data[id_column]
+#     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+#     return data.loc[~in_test_set],data.loc[in_test_set]
 
 
-def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
-    return data.loc[~in_test_set],data.loc[in_test_set]
-
-
-housing_with_id = housing.reset_index()
+# housing_with_id = housing.reset_index()
 # train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
-housing_with_id["id"] = housing["longitude"] * 1000 + housing_with_id["latitude"]
-train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+# housing_with_id["id"] = housing["longitude"] * 1000 + housing_with_id["latitude"]
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
 
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
+housing["income_cat"] = np.ceil(housing["median_income"]/1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing,housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+# print(housing["income_cat"].value_counts() / len(housing))
+for set in (strat_train_set, strat_test_set):
+    set.drop(["income_cat"], axis=1, inplace=True)
+
+# housing = strat_train_set.copy()
+# housing.plot(kind="scatter",x="longitude",y="latitude", alpha=0.4,
+#              s=housing["population"]/100, label="population",
+#              c="median_house_value",cmap=plt.get_cmap("jet"),colorbar=True)
+# plt.show()
+
+# corr_matrix = housing.corr()
+# corr_matrix["median_house_value"].sort_values(ascending=False)
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+# attributes = ["median_house_value","median_income", "total_rooms","housing_median_age"]
+# scatter_matrix(housing[attributes],figsize=(12,8))
+#
+# housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+# plt.show()
+
+# housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
+# housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
+# housing["population_per_household"] = housing["population"] / housing["households"]
+# corr_matrix = housing.corr()
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+# housing.dropna(subset=["total_bedrooms"])
+# housing.drop("total_bedrooms",axis=1)
+# median = housing["total_bedrooms"].median()
+# housing["total_bedrooms"].fillna(median)
+
+simple_imputer = SimpleImputer(strategy="median")
+housing_num = housing.drop("ocean_proximity", axis=1)
+simple_imputer.fit(housing_num)
+# print(simple_imputer.statistics_)
+# print(housing_num.median().values)
+X = simple_imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
+# encoder = LabelEncoder()
+housing_cat = housing["ocean_proximity"]
+# housing_cat_encoded = encoder.fit_transform(housing_cat)
+# print(housing_cat_encoded)
+# print(encoder.classes_)
+# encoder = OneHotEncoder()
+# housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1, 1))
+# print(housing_cat_1hot)
+# print(housing_cat_1hot.toarray())
+encoder = LabelBinarizer(sparse_output=True)
+housing_cat_1hot = encoder.fit_transform(housing_cat)
+# print(housing_cat_1hot)
 
 
 
