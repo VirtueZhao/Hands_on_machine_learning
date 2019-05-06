5a04ded2cadd0db50def0c549fa2b47c5a4c5bd6
DiZhao <zd277607529@gmail.com>
2019-05-06 22:05:52 +1200

    update

--- Chapter3/mnist.py
+++ Chapter3/mnist.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import fetch_mldata
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
@@ -12,6 +13,7 @@ from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.multiclass import OneVsOneClassifier
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -33,11 +35,11 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 shuffle_index = np.random.permutation(60000)
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 
-y_train_5 = (y_train == 5)
-y_test_5 = (y_test == 5)
-
+# y_train_5 = (y_train == 5)
+# y_test_5 = (y_test == 5)
+#
 sgd_clf = SGDClassifier(random_state=42)
-sgd_clf.fit(X_train, y_train_5)
+# sgd_clf.fit(X_train, y_train_5)
 
 # print(sgd_clf.predict([some_digit]))
 
@@ -66,27 +68,27 @@ class Never5Classfier(BaseEstimator):
         return np.zeros((len(X), 1), dtype=bool)
 
 
-never_5_clf = Never5Classfier()
+# never_5_clf = Never5Classfier()
 # print(cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
 
-y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+# y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 # print(confusion_matrix(y_train_5, y_train_pred))
 # print(precision_score(y_train_5, y_train_pred))
 # print(recall_score(y_train_5, y_train_pred))
 # print(f1_score(y_train_5, y_train_pred))
 
-y_scores = sgd_clf.decision_function([some_digit])
-print(y_scores)
-threshold = 0
-y_some_digit_pred = (y_scores > threshold)
+# y_scores = sgd_clf.decision_function([some_digit])
+# print(y_scores)
+# threshold = 0
+# y_some_digit_pred = (y_scores > threshold)
 # print(y_some_digit_pred)
-threshold = 200000
-y_some_digit_pred = (y_scores > threshold)
+# threshold = 200000
+# y_some_digit_pred = (y_scores > threshold)
 # print(y_some_digit_pred)
 
-y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
+# y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
 # print(y_scores)
-precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+# precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
 
 
 def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
@@ -105,7 +107,7 @@ def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
 # print(precision_score(y_train_5, y_train_pred_90))
 # print(recall_score(y_train_5, y_train_pred_90))
 
-fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+# fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
 
 
 def plot_roc_curve(fpr, tpr, label=None):
@@ -121,10 +123,10 @@ def plot_roc_curve(fpr, tpr, label=None):
 
 # print(roc_auc_score(y_train_5, y_scores))
 
-forest_clf = RandomForestClassifier(random_state=42)
-y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
-y_scores_forest = y_probas_forest[:, 1]
-fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
+# forest_clf = RandomForestClassifier(random_state=42)
+# y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
+# y_scores_forest = y_probas_forest[:, 1]
+# fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
 
 # plt.plot(fpr, tpr, "b:", label="SGD")
 # plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
@@ -135,8 +137,37 @@ fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest
 # print(recall_score(y_train_5, y_train_pred))
 
 sgd_clf.fit(X_train, y_train)
-print(sgd_clf.predict([some_digit]))
-some_digit_scores = sgd_clf.decision_function([some_digit])
-print(some_digit_scores)
-print(np.argmax(some_digit_scores))
-print(sgd_clf.classes_[5])
+# print(sgd_clf.predict([some_digit]))
+# some_digit_scores = sgd_clf.decision_function([some_digit])
+# print(some_digit_scores)
+# print(np.argmax(some_digit_scores))
+# print(sgd_clf.classes_[5])
+
+# ovo_clf = OneVsOneClassifier(SGDClassifier(random_state=42))
+# ovo_clf.fit(X_train, y_train)
+# print(ovo_clf.predict([some_digit]))
+# print(len(ovo_clf.estimators_))
+
+# forest_clf.fit(X_train, y_train)
+# print(forest_clf.predict([some_digit]))
+# print(forest_clf.predict_proba([some_digit]))
+
+# print(cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring="accuracy"))
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train.astype(np.int))
+# print(cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring="accuracy"))
+
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+conf_mx = confusion_matrix(y_train, y_train_pred)
+# print(conf_mx)
+# plt.matshow(conf_mx, cmap=plt.cm.gray)
+# plt.show()
+
+# row_sums = conf_mx.sum(axis=1, keepdims=True)
+# norm_conf_mx = conf_mx / row_sums
+# np.fill_diagonal(norm_conf_mx, 0)
+# plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
+# plt.show()
+
+
