51b48a9cb597cfaf13906dd9aa68bab60ab85f75
DiZhao <zd277607529@gmail.com>
2019-06-13 21:25:30 +1200

    update

--- Chapter4/ch4.py
+++ Chapter4/ch4.py
@@ -132,9 +132,9 @@ def plot_learning_curves(model, X, y):
 # sgd_reg.fit(X, y.ravel())
 # print(sgd_reg.predict([[1.5]]))
 
-elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
-elastic_net.fit(X, y)
-print(elastic_net.predict([[1.5]]))
+# elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+# elastic_net.fit(X, y)
+# print(elastic_net.predict([[1.5]]))
 
 
 
