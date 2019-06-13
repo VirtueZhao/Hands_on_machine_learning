1a205b268b949fae6535e45d871a9be1fbd497c4
DiZhao <zd277607529@gmail.com>
2019-06-13 21:06:41 +1200

    update

--- Chapter4/ch4.py
+++ Chapter4/ch4.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
@@ -115,19 +116,25 @@ def plot_learning_curves(model, X, y):
 #
 # plot_learning_curves(polynomial_regression, X, y)
 
-ridge_reg = Ridge(alpha=1, solver="cholesky")
-ridge_reg.fit(X, y)
-print(ridge_reg.predict([[1.5]]))
+# ridge_reg = Ridge(alpha=1, solver="cholesky")
+# ridge_reg.fit(X, y)
+# print(ridge_reg.predict([[1.5]]))
+#
+# sgd_reg = SGDRegressor(penalty="l2")
+# sgd_reg.fit(X, y.ravel())
+# print(sgd_reg.predict([[1.5]]))
+#
+# lasso_reg = Lasso(alpha=0.1)
+# lasso_reg.fit(X, y)
+# print(lasso_reg.predict([[1.5]]))
+#
+# sgd_reg = SGDRegressor(penalty="l1")
+# sgd_reg.fit(X, y.ravel())
+# print(sgd_reg.predict([[1.5]]))
 
-sgd_reg = SGDRegressor(penalty="l2")
-sgd_reg.fit(X, y.ravel())
-print(sgd_reg.predict([[1.5]]))
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+print(elastic_net.predict([[1.5]]))
 
-lasso_reg = Lasso(alpha=0.1)
-lasso_reg.fit(X, y)
-print(lasso_reg.predict([[1.5]]))
 
-sgd_reg = SGDRegressor(penalty="l1")
-sgd_reg.fit(X, y.ravel())
-print(sgd_reg.predict([[1.5]]))
 
