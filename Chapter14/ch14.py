faa46d0455061323a925755b15bc108e2e3aa15c
DiZhao <zd277607529@gmail.com>
2019-07-24 14:51:51 +1200

    train rnn time series

--- Chapter14/ch14.py
+++ Chapter14/ch14.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
-n_inputs = 3
-n_neurons = 5
+# n_inputs = 3
+# n_neurons = 5
 #
 # X0 = tf.placeholder(tf.float32, [None, n_inputs])
 # X1 = tf.placeholder(tf.float32, [None, n_inputs])
@@ -69,7 +69,30 @@ n_neurons = 5
 #
 # print(outputs_val)
 
+n_inputs = 3
+n_neurons = 5
+n_steps = 2
+
 seq_length = tf.placeholder(tf.int32,[None])
+basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
 
+outputs, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32, sequence_length=seq_length)
 
+init = tf.global_variables_initializer()
+
+X_batch = np.array([
+    [[0, 1, 2], [9, 8, 7]],
+    [[3, 4, 5], [0, 0, 0]],
+    [[6, 7, 8], [6, 5, 4]],
+    [[9, 0, 1], [3, 2, 1]],
+])
+seq_length_batch = np.array([2, 1, 2, 2])
+
+with tf.Session() as sess:
+    init.run()
+    outputs_val, states_val = sess.run([outputs, states], feed_dict={X:X_batch, seq_length:seq_length_batch})
+
+print(outputs_val)
+print(states_val)
 
