64a2548b8442b2c34bc267000e44fa636c818732
DiZhao <zd277607529@gmail.com>
2019-07-30 15:54:20 +1200

    stacked_autoencoder

---
+++ Chapter15/ch15.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.contrib.layers import fully_connected
+
+n_inputs = 3
+n_hidden = 2
+n_outputs = n_inputs
+
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+hidden = fully_connected(X, n_hidden, activation_fn=None)
+outputs = fully_connected(hidden, n_outputs, activation_fn=None)
+
+reconstruction_loss = tf.reduce_mean(tf.squart(outputs - X))
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
+training_op = optimizer.minimize(reconstruction_loss)
+
+init = tf.global_variables_initializer()
+
+X_train, X_test = []
+
+n_interations = 1000
+codings = hidden
+
+with tf.Session() as sess:
+    init.run()
+    for iteration in range(n_interations):
+        training_op.run(feed_dict={X: X_train})
+    codings_val = codings.eval(feed_dict={X: X_test})
+
